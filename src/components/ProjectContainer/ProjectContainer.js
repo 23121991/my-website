b92b135a2d2766de2ab6ba46e4cb232e2384ce53
SARAVANAN <sb9489.rs@gmail.com>
2023-02-16 02:53:54 +0530

    new

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -6,35 +6,35 @@ import "./ProjectContainer.css";
 const ProjectContainer = () => {
   const projects = [
     {
-      img: "../assets/gmail.jpg",
+      img: "./assets/gmail_clone.png",
       title: "Gmail Clone",
       desc: "gmail clone app using React,Mongodb & Nodejs",
       link: "https://regal-liger-dce38b.netlify.app/",
 
     },
     {
-      img: "../assets/quotesapp.jpg",
+      img: "./assets/quotes.png",
       title: "Quotes App",
       desc: "Quotes App using frontend Reactjs with backend Mongodb & Nodejs",
       link: "https://master--reliable-medovik-bb2dd4.netlify.app/",
     },
     
     {
-      img: "../assets/covid_tracker.png",
+      img: "./assets/todolist.png",
       title: "ToDoList-app",
       desc: "ToDoList-app using frontend Reactjs with backend Mongodb & Nodejs",
       link: "https://master--papaya-moonbeam-cd5f86.netlify.app/",
 
     },
     {
-      img: "../assets/zomato.png",
+      img: "./assets/zomato.png",
       title: "Zomato",
       desc: "zomato search-filter-app",
       link: "https://spontaneous-arithmetic-cc6a4e.netlify.app/",
 
     },
     {
-      img: "../assets/form.png",
+      img: "./assets/form.png",
       title: "form",
       desc: "creating form using reactjs ",
       link: "https://main--comfy-churros-d38d02.netlify.app/",
